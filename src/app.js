60749f97d5fa4dd6e272e825df5e86c5b7e8aabc
josemaria1988 <idontknow1988@gmail.com>
2023-03-18 00:19:52 -0300

    actividad en clase practicada

--- src/app.js
+++ src/app.js
@@ -3,23 +3,43 @@ import productsRouter from './routes/products.routes.js';
 import cartsRouter from './routes/cart.routes.js';
 import handlebars from "express-handlebars";
 import __dirname from "./utils.js";
+import {Server} from 'socket.io';
+import viewsRouter from './routes/views.routes.js';
 
 const app = express();
 const port = 8080;
 
+app.use(express.static(`${__dirname}/public`))
 app.use(express.json())
-app.engine("handlebars", handlebars.engine());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api/products', productsRouter);
-
-app.use('/api/carts', cartsRouter);
-
 app.engine("handlebars", handlebars.engine());
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+app.use("/", viewsRouter);
 
 
-app.listen(port, () => {
+
+const httpServer = app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
 });
+
+const socketServer = new Server(httpServer);
+
+let logs = [];
+
+socketServer.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado");
+
+  socket.on("message1", (data) => {
+    console.log(data);
+  })
+
+  socket.on("message2", (data) => {
+    logs.push({socketId: socket.id, message: data});
+    socketServer.emit("log", {logs});
+  })
+
+  socketServer.emit('message2', 'Hola mundo desde socket backend!')
+})
